fb740de8ef2d4aa8a4638c118af686f92e7b19a0
VeronikaVasilchikova <veroni4ka.v@gmail.com>
2020-06-11 10:10:27 +0300

    Add a Chart to StoryBook

---
+++ src/components/chart/ChartConfigurated.vue
@@ -0,0 +1,35 @@
+<template>
+	<section style="width: 600px">
+		<div ref="chart"></div>
+	</section>
+</template>
+
+<script>
+import { Chart as ChartDHX } from "dhx-suite";
+export default {
+	name: "ChartConfigurated",
+	data: () => ({
+    chart: null
+	}),
+	props: {
+		options: {
+			type: Object,
+			required: true
+		}
+	},
+  mounted() {
+    this.chart = new ChartDHX(this.$refs.chart, {
+      ...this.options
+    });
+
+    this.chart.data.load("https://dhtmlx.github.io/react-widgets/static/chart.json");
+  },
+  beforeDestroy() {
+		if (this.chart && this.chart.destroy) {
+      this.chart.destroy();
+    }
+	}
+}
+</script>
+
+
