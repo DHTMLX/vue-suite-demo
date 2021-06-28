933418952c1cb4049359908d645f0246b8ff5134
Sorokin-Oleg <sorokin.world@gmail.com>
2021-06-28 19:51:53 +0300

    [dev] Toolbar. FIx data import

--- src/components/toolbar/ToolbarCdn.vue
+++ src/components/toolbar/ToolbarCdn.vue
@@ -4,20 +4,18 @@
 
 <script>
 import fromCDN from "from-cdn";
-import { TreeCollection } from "dhx-suite";
+
 export default {
   name: "ToolbarCdn",
   data: () => ({
     toolbar: null,
-    data: new TreeCollection(),
   }),
   mounted() {
     fromCDN(["https://cdn.dhtmlx.com/suite/edge/suite.js", "https://cdn.dhtmlx.com/suite/edge/suite.css"]).then(() => {
-      // eslint-disable-next-line no-undef
+      // eslint-disable-next-line
       this.toolbar = new dhx.Toolbar(this.$refs.toolbar, {
         css: "dhx_widget--bordered dhx_widget--bg_white",
         navigationType: "pointer",
-        data: this.data,
       });
       this.toolbar.data.load("https://dhtmlx.github.io/react-widgets/static/toolbar.json");
     });
